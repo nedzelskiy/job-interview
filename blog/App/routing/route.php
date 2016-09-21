52af479611a64feea7e72b6238ff55a4270a34f1
Pavel <pavel@fironet.com.ua>
2016-09-21 18:09:59 +0300

    continue

--- blog/App/routing/route.php
+++ blog/App/routing/route.php
@@ -4,6 +4,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Silex\Application;
 
-$app->get('/blog', function() use ($app) {
-    return 'yes';
+$app->get('/', function() use ($app) {
+    return $app->redirect($app['url_generator']->generate('allrecords'));
 });
+
+$app->get('/blog', function() use ($app) {
+    return $app['twig']->render('allrecords.twig');
+})->bind('allrecords');
